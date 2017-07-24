8d0ec77dfa990af29fe7034af67c65e4f746aad9
nainireddy <nainivenkat@gmail.com>
2017-07-23 19:24:41 -0700

    Add files via upload

--- excercises/atoi.py
+++ excercises/atoi.py
@@ -1,11 +1,24 @@
+def isNumericChar(x):
+    if (x >= '0' and x <= '9'):
+        return True
+    return False
+
 def myAtoi(string):
     res = 0
-    # Iterate through all characters of input string and
-    # update result
-    for i in xrange(len(string)):
+    sign = 1
+    i = 0
+    if string[0] == '-':
+        sign = -1
+        i = i+1
+    
+    for i in xrange(i, len(string)):
+        if isNumericChar(string[i] == False):
+            return 0
+        
         res = res*10 + (ord(string[i]) - ord('0'))
-    return res
+
+    return res*sign
 
 # Driver program
-string = "89789"
+string = "-134"
 print myAtoi(string)
