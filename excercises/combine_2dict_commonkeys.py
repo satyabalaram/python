fb2a8c50fa3c5b97029acf8d6542e2b030419770
nainireddy <nainivenkat@gmail.com>
2017-10-12 14:31:33 -0700

    Update combine_2dict_commonkeys.py
    Modified the program to do with and without using collections module

--- excercises/combine_2dict_commonkeys.py
+++ excercises/combine_2dict_commonkeys.py
@@ -1,20 +1,19 @@
 from collections import Counter
-
 d1 = {'a': 100, 'b': 200, 'c':300}
 d2 = {'a': 300, 'b': 200, 'd':400}
 
-c = dict()
-for keys, values in d1.items():
-    for keys1, values1 in d2.items():
-        if keys1 ==  keys:
-            c[keys]= d1[keys] + d2[keys1]
-        elif keys1 not in d1.keys() and keys not in d2.keys():
-            c[keys1] = d2[keys1]
-            c[keys] = d1[keys]
-       
-print c
-        
-d = Counter(d1) + Counter(d2)
-print d
-        
-        
+d3 = {}
+for key, value in d1.items():
+    for key1,value1 in d2.items():
+        if key == key1:
+            d3[key] = value + value1
+        elif key not in d3.keys():
+            d3[key] = value
+        elif key1 not in d3.keys():
+            d3[key1] = value1
+
+print d3
+
+d4 = {}
+d4 = Counter(d1) + Counter(d2)
+print dict(d4)
